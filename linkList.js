dcdea63aa225f8022e7cb9fba9eafe64e98f99c7
alaminislamrahat <alaminislamrahat@gmail.com>
2026-01-04 23:43:26 +0600

    insert method work done

--- linkList.js
+++ linkList.js
@@ -1,5 +1,5 @@
 class Node {
-    constructor(value){
+    constructor(value) {
         this.value = value;
         this.next = null;
     }
@@ -7,53 +7,96 @@ class Node {
 
 
 class LinkedList {
-    constructor(){
+    constructor() {
         this.head = null;
         this.tail = null;
         this.length = 0;
     }
 
-    append(value){
+    append(value) {
 
-         const newNode = new Node(value)
+        const newNode = new Node(value)
         //? if LinkedList is empty
-        if(this.head === null){
-           this.head = newNode;
-           this.tail = newNode;
-        }else{
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
             //! if linkedList not empty
             this.tail.next = newNode;
             this.tail = newNode;
         }
 
         this.length++;
+        return this;
     }
 
-    prepend(value){
-         const newNode = new Node(value)
+    prepend(value) {
+        const newNode = new Node(value)
         //? if LinkedList is empty
-        if(this.head === null){
-           this.head = newNode;
-           this.tail = newNode;
-        }else{
+        if (this.head === null) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
             //! if linkedList not empty
             newNode.next = this.head;
             this.head = newNode;
         }
 
         this.length++;
+        return this;
     }
 
-    insert(){}
+    insert(index, value) {
+        if (index < 0 || index > this.length) {
+            console.log("index out of bound");
+            return undefined;
+        }
 
-    remove(){}
+        //if the insert is in the fist of the linked list
+        if (index === 0) {
+            return this.prepend(value)
+        }
 
-    print(){
+        // if the insert is in the end of the linkedlist 
+        if (index === this.length) {
+            return this.append(value)
+        }
+
+        // if the insert is in the middle 
+
+        //? find the leading node
+        const leadingNode = this._traverseToIndex(index - 1)
+        const holdingNode = leadingNode.next;
+
+        const newNode = new Node(value);
+        leadingNode.next = newNode;
+        newNode.next = holdingNode
+        this.length++;
+
+
+    }
+
+    remove() { }
+
+    //* private helper method
+    _traverseToIndex(index) {
+        let count = 0;
+        let currentNode = this.head;
+
+        while (count !== index) {
+            currentNode = currentNode.next;
+            count++
+        }
+
+        return currentNode;
+    }
+
+    print() {
 
         const arr = [];
 
         let currentNode = this.head;
-        while(currentNode !== null){
+        while (currentNode !== null) {
             arr.push(currentNode.value);
             currentNode = currentNode.next;
         }
@@ -64,13 +107,12 @@ class LinkedList {
 
 
 const linkedList = new LinkedList();
+linkedList.append(0)
 linkedList.append(1)
-linkedList.append(2)
 linkedList.append(3)
 
-linkedList.prepend(10)
-linkedList.prepend(20)
-linkedList.prepend(30)
+linkedList.insert(2,200)
+
 
 linkedList.print()
 
